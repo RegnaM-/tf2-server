56944fc1093dc942e2b81821a95416cfe02cda2d
RegnaM- <regnam@gmail.com>
2018-08-02 22:50:41 +0200

    Update start.sh

--- containerfs/csgo/start.sh
+++ containerfs/csgo/start.sh
@@ -34,10 +34,12 @@ else
     export SERVER_HOSTNAME="${SERVER_HOSTNAME:-An Amazing CSGO Server}"
     export SERVER_PASSWORD="${SERVER_PASSWORD:-changeme}"
     export RCON_PASSWORD="${RCON_PASSWORD:-changeme}"
+    export GOTV_HOSTNAME="${GOTV_HOSTNAME:-GOTV NAME}"
     export STEAM_ACCOUNT="${STEAM_ACCOUNT:-changeme}"
     export CSGO_DIR="${CSGO_DIR:-/csgo}"
     export IP="${IP:-0.0.0.0}"
     export PORT="${PORT:-27015}"
+    export GOTV_PORT="${GOTV_PORT:-27020}"
     export TICKRATE="${TICKRATE:-128}"
     export GAME_TYPE="${GAME_TYPE:-0}"
     export GAME_MODE="${GAME_MODE:-1}"
@@ -55,8 +57,11 @@ cat << SERVERCFG > $CSGO_DIR/csgo/cfg/server.cfg
 hostname "$SERVER_HOSTNAME"
 rcon_password "$RCON_PASSWORD"
 sv_password "$SERVER_PASSWORD"
+tv_name "$GOTV_NAME"
 sv_lan 0
 sv_cheats 0
+exec gotv.cfg
+
 SERVERCFG
 
 ./srcds_run \
@@ -65,6 +70,8 @@ SERVERCFG
     -game csgo \
     -tickrate $TICKRATE \
     -port $PORT \
+    +tv_port $GOTV_PORT \
+    -port $GOTV_NAME \
     -maxplayers_override $MAXPLAYERS \
     +game_type $GAME_TYPE \
     +game_mode $GAME_MODE \
